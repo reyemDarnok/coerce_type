806da4c9f91cb9113b7adbab244a67f3f4135add
Ben Konrad Meyer <ben.k.meyer@gmail.com>
2026-03-17 13:21:09 +0100

    typing unions support

--- src/coerce_type/coercion.py
+++ src/coerce_type/coercion.py
@@ -1,30 +1,44 @@
-from os import GenericAlias
-from typing import Any, Type, TypeVar, get_origin, get_args
+from types import UnionType, NoneType
+from typing import Any, Type, TypeVar, get_args, get_origin, Union
 
 T = TypeVar("T")
 
 
 def coerce(
     obj: Any,
-    typ: Type[T],
+    type_: Type[T] | UnionType,
     *,
     lower_true_strings: tuple[str] = ("true", "t", "1", "yes", "y", "on"),
     str_truthiness: bool = False,
 ) -> T:
     kwargs = {"lower_true_strings": lower_true_strings, "str_truthiness": str_truthiness}
-    if get_origin(typ) == list:
-        member_type = get_args(typ)[0]
+    if get_origin(type_) == list:
+        member_type = get_args(type_)[0]
         return [coerce(member, member_type, **kwargs) for member in obj]
-    if get_origin(typ) == dict:
-        key_type, value_type = get_args(typ)
+    if get_origin(type_) == dict:
+        key_type, value_type = get_args(type_)
         return {coerce(key, key_type, **kwargs): coerce(value, value_type, **kwargs) for key, value in obj.items()}
-    if isinstance(obj, typ):
+    if get_origin(type_) == Union:
+        # prioritize keeping None as none and not coerce it to something else
+        if NoneType in get_args(type_):
+            try:
+                return coerce(obj, NoneType, **kwargs)
+            except ValueError:
+                pass
+        for type_in_union in get_args(type_):
+            try:
+                return coerce(obj, type_in_union, **kwargs)
+            except ValueError:
+                pass
+        else:
+            raise ValueError(f"Cannot coerce {obj} to {type_}")
+    if isinstance(obj, type_):
         return obj
-    if typ in (int, float, str):
-        return typ(obj)
-    if typ is bool:
+    if type_ in (int, float, str):
+        return type_(obj)
+    if type_ is bool:
         if isinstance(obj, str) and not str_truthiness:
             return obj.lower() in lower_true_strings
         else:
             return bool(obj)
-    raise ValueError(f"Cannot coerce {obj} to {typ}")
+    raise ValueError(f"Cannot coerce {obj} to {type_}")
--- tests/test_coercion.py
+++ tests/test_coercion.py
@@ -24,28 +24,46 @@ import coerce_type
 def test_default_args(obj, typ, result) -> None:
     assert coerce_type.coerce(obj, typ) == result
 
-
-def test_non_str_bool_conversion() -> None:
-    assert coerce_type.coerce([1], bool)
-    assert not coerce_type.coerce([], bool)
-
-
-def test_str_truthiness() -> None:
-    assert coerce_type.coerce("False", bool, str_truthiness=True)
-    assert not coerce_type.coerce("", bool, str_truthiness=True)
-
-
-def test_custom_str_truthiness() -> None:
-    assert coerce_type.coerce("Really", bool, lower_true_strings=("really",))
-    assert not coerce_type.coerce("Nah", bool, lower_true_strings=("really",))
-    assert not coerce_type.coerce("True", bool, lower_true_strings=("really",))
+@pytest.mark.parametrize(
+    "obj, result", [
+        ([1], True),
+        ([], False)
+    ]
+)
+def test_non_str_bool_conversion(obj, result) -> None:
+    assert coerce_type.coerce(obj, bool) == result
 
 
-def test_custom_str_truthiness_is_overridden() -> None:
-    assert coerce_type.coerce("Really", bool, str_truthiness=True, lower_true_strings=("really",))
-    assert coerce_type.coerce("Nah", bool, str_truthiness=True, lower_true_strings=("really",))
-    assert coerce_type.coerce("a", bool, str_truthiness=True, lower_true_strings=("really",))
-    assert not coerce_type.coerce("", bool, str_truthiness=True, lower_true_strings=("really",))
+@pytest.mark.parametrize(
+    "obj, result", [
+        ("False", True),
+        ("", False)
+    ]
+)
+def test_str_truthiness(obj, result) -> None:
+    assert coerce_type.coerce(obj, bool, str_truthiness=True) == result
+
+@pytest.mark.parametrize(
+    "obj, result", [
+        ("Really", True),
+        ("Nah", False),
+        ("True", False),
+    ]
+)
+def test_custom_str_truthiness(obj, result) -> None:
+    assert coerce_type.coerce(obj, bool, lower_true_strings=("really",)) == result
+
+@pytest.mark.parametrize(
+    "obj, result",[
+        ("Really", True),
+        ("Nah", True),
+        ("a", True),
+        ("", False),
+    ]
+)
+def test_custom_str_truthiness_is_overridden(obj, result) -> None:
+    assert coerce_type.coerce(obj, bool, str_truthiness=True, lower_true_strings=("really",)) == result
+
 
 
 def test_fail_builtin() -> None:
@@ -60,15 +78,42 @@ def test_fail_non_builtin() -> None:
     with pytest.raises(ValueError):
         coerce_type.coerce("some text", CustomClass)
 
+
 def test_typing_lists() -> None:
-    assert coerce_type.coerce([1,2,3], list[str]) == ["1", "2", "3"]
+    assert coerce_type.coerce([1, 2, 3], list[str]) == ["1", "2", "3"]
+
 
 def test_typing_list_kwargs() -> None:
-    assert coerce_type.coerce(["Really",0,1], list[bool], lower_true_strings=("really",)) == [True, False, True]
+    assert coerce_type.coerce(["Really", 0, 1], list[bool], lower_true_strings=("really",)) == [True, False, True]
+
 
 def test_typing_dicts() -> None:
     assert coerce_type.coerce({"0.2": "1", "0.5": "2"}, dict[float, int]) == {0.2: 1, 0.5: 2}
 
+
 def test_typing_dicts_kwargs() -> None:
-    assert coerce_type.coerce({"0.2": "1", "0.5": "2"}, dict[float, bool],
-                              lower_true_strings=("2",)) == {0.2: False, 0.5: True}
+    assert coerce_type.coerce({"0.2": "1", "0.5": "2"}, dict[float, bool], lower_true_strings=("2",)) == {
+        0.2: False,
+        0.5: True,
+    }
+
+@pytest.mark.parametrize(
+    "value, type_, result", [
+        ("True", int | bool, True),
+        ("4", int | bool, 4),
+    ]
+)
+def test_union_of_existing(value, type_, result) -> None:
+    assert coerce_type.coerce(value, type_) == result
+
+def test_union_fail() -> None:
+    with pytest.raises(ValueError):
+        coerce_type.coerce("some text", int | float)
+
+def test_optional():
+    assert coerce_type.coerce(None, int | None) is None
+
+def test_optional_fail() -> None:
+    with pytest.raises(ValueError):
+        assert coerce_type.coerce("some text", int | None)
+
