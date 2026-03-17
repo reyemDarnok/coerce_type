b3618fbc38010df612a27c5e50b4bfda46c73374
Ben Konrad Meyer <ben.k.meyer@gmail.com>
2026-03-17 10:46:46 +0100

    actual conversions to builtins

--- src/coerce_type/coercion.py
+++ src/coerce_type/coercion.py
@@ -3,7 +3,20 @@ from typing import Any, Type, TypeVar
 T = TypeVar("T")
 
 
-def coerce(obj: Any, typ: Type[T]) -> T:
+def coerce(
+    obj: Any,
+    typ: Type[T],
+    *,
+    lower_true_strings: tuple[str] = ("true", "t", "1", "yes", "y", "on"),
+    str_truthiness: bool = False,
+) -> T:
     if isinstance(obj, typ):
         return obj
+    if typ in (int, float, str):
+        return typ(obj)
+    if typ is bool:
+        if isinstance(obj, str) and not str_truthiness:
+            return obj.lower() in lower_true_strings
+        else:
+            return bool(obj)
     raise ValueError(f"Cannot coerce {obj} to {typ}")
--- tests/test_coercion.py
+++ tests/test_coercion.py
@@ -6,14 +6,23 @@ import coerce_type
 @pytest.mark.parametrize(
     "obj, typ, result",
     [
+        # no-op coercions
         (5, int, 5),
         (0.2, float, 0.2),
         ("text", str, "text"),
+        (True, bool, True),
+        # coercions of build in types
+        (1.0, int, 1),
+        ("10", int, 10),
+        ("0.2", float, 0.2),
+        ("True", bool, True),
+        ("False", bool, False),
+        (5, str, "5"),
+        (0.2, str, "0.2"),
     ],
 )
-def test_coerce(obj, typ, result) -> None:
+def test_coerce_default_args(obj, typ, result) -> None:
     assert coerce_type.coerce(obj, typ) == result
-
 
 def test_coerce_fail() -> None:
     with pytest.raises(ValueError):
